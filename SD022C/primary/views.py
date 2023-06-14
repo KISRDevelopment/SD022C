f2168222bcee85389e59dc2bafe4fedac7240527
Zahraa Ali <zahraaali@outlook.com>
2023-06-14 20:26:00 +0300

    new app created called primary

---
+++ SD022C/primary/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index (request):
+    return render (request,"primary/index.html")
+def help (request):
+    return render (request,"primary/help.html")
+
+def login (request):
+    return render (request,"primary/login.html")
+
+def about (request):
+    return render (request,"primary/about.html")
+    
+def contact (request):
+    return render (request,"primary/contact.html")

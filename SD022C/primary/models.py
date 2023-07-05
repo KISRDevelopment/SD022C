bd983b18a4e3e5c97e83ffcf8c41b80b7382e663
Zahraa A. Ali <zali@kisr.edu.kw>
2023-07-05 14:34:51 +0300

    login interface and new superuser creation

--- SD022C/primary/models.py
+++ SD022C/primary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Superusers(models.Model):
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=50)
+    confirm_password = models.CharField(max_length=50)
+    name = models.CharField(max_length=60)
+    speciality = models.CharField(max_length=60)
+    organization = models.CharField(max_length=60)
--- SD022C/primary/views.py
+++ SD022C/primary/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
+from .models import Superusers
+
 # Create your views here.
 def index (request):
     return render (request,"primary/index.html")
+
+def adminPage (request):
+    return render (request,"primary/adminPage.html")
+
+def signupSuperUser (request):
+    return render (request,"primary/signupSuperUser.html")
+
 def help (request):
     return render (request,"primary/help.html")
 
@@ -17,3 +28,48 @@ def contact (request):
 
 def requestPage (request):
     return render (request,"primary/requestPage.html")
+
+def superusers (request):
+    return render (request,"primary/superusers.html")
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("primary/adminPage.html")
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect("primary/login.html")
+    else:
+        return render(request, "primary/login.html")
+
+def registerSuperUser(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        name = request.POST['name']
+        speciality = request.POST['speciality']
+        organization = request.POST['organization']
+
+        if password==confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is already taken')
+                return redirect("primary/signupSuperUser.html")
+            else:
+                user = User.objects.create_user(username=username, password=password, 
+                                name=name, speciality=speciality, organization=organization)
+                user = Superusers(username="username",password="password",name="name", speciality="speciality", organization="organization")
+                user.save()
+                return redirect("primary/superusers.html")
+        else:
+            messages.info(request, 'Both passwords are not matching')
+            return redirect("primary/signupSuperUser.html")
+            
+    else:
+        return render(request, "primary/signupSuperUser.html")

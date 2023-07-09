5f81fd6f12c75cfdc851d65cccf8303e740d2cf8
Zahraa A. Ali <zali@kisr.edu.kw>
2023-07-09 14:33:55 +0300

    edit and delete btn for each superuser

--- SD022C/primary/models.py
+++ SD022C/primary/models.py
@@ -10,4 +10,4 @@ class Superusers(models.Model):
     organization = models.CharField(max_length=60)
 
     def __str__(self):
-        return f"{self.id}: {self.username} to {self.organization}"
+        return f"{self.id}: {self.username}"

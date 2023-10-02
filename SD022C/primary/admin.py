4babe3f76650507f17907aae67e38ccc4b4d5860
Zahraa A. Ali <zali@kisr.edu.kw>
2023-10-02 14:31:30 +0300

    Student Table added to admin.py

--- SD022C/primary/admin.py
+++ SD022C/primary/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Examiner
+from .models import Student
 # Register your models here.
 admin.site.register(Examiner)
-
+admin.site.register(Student)

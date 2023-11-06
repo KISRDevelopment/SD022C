30a1e000ef742541a9bacfefc55925a4860f82ca
Zahraa A. Ali <zali@kisr.edu.kw>
2023-11-06 09:41:44 +0300

    added next section

--- SD022C/primary/urls.py
+++ SD022C/primary/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('deleteStudent/<int:id>', views.deleteStudent, name='deleteStudent'),
     path('testsPage/', views.testsPage, name='testsPage'),
     path('startTest/', views.startTest, name='startTest'),
-    path('rapidNamingObj/', views.rapidNamingObj, name='rapidNamingObj'),
+    path('rapidNamingObjTst/', views.rapidNamingObjTst, name='rapidNamingObjTst'),
     #path('editStudent/<int:id>', views.editStudent, name='editStudent'),
 
     #path('password/<int:id>', views.password, name='password'),
--- SD022C/primary/views.py
+++ SD022C/primary/views.py
@@ -146,10 +146,10 @@ def startTest(request):
     return render(request, "primary/students.html")
 
 @login_required(login_url="/primary/login")
-def rapidNamingObj(request):
+def rapidNamingObjTst(request):
     if request.method == "POST":
-        return redirect('primary:rapidNamingObj')
-    return render(request, "primary/rapidNamingObj.html")
+        return redirect('primary:rapidNamingObjTst')
+    return render(request, "primary/rapidNamingObjTst.html")
 
 
 """ @login_required(login_url="/primary/login")

0258c32f8bf86bce33a8ffc7267cf6df2ccf74a5
Zahraa Ali <zahraaali@outlook.com>
2023-12-17 20:47:49 +0300

    change status to arabic

--- SD022C/primary/views.py
+++ SD022C/primary/views.py
@@ -338,7 +338,7 @@ def testsPage (request):
         scoreB=wrongB+durationB
         total=scoreA+scoreB
         return render(request,"primary/testsPage.html", {
-            "totalScore":(round(total)), "status":('Done') , "student":(Score.objects.get(student_id=request.session['student']).student),     
+            "totalScore":(round(total)), "status":('تم الانتهاء') , "student":(Score.objects.get(student_id=request.session['student']).student),     
         })
     else:
         return render(request,"primary/testsPage.html")

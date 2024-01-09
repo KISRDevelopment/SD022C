b6e34c50a6b9a933198a8d407f5ff1d6b7a29892
smerri <smerri@kisr.edu.kw>
2024-01-09 22:30:24 +0300

    admin.py table

--- SD022C/primary/admin.py
+++ SD022C/primary/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Examiner
 from .models import Student
 from .models import Score
+from .models import *
 # Register your models here.
 admin.site.register(Examiner)
 admin.site.register(Student)
 admin.site.register(Score)
+admin.site.register(Test)
